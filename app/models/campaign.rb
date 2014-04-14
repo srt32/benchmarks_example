ddf16cab6246b146c742b03787bac6195504610d
Simon Taranto <simon@thoughtbot.com>
2014-04-14 10:42:11 -0700

    add funds_raised method

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,7 @@
 class Campaign < ActiveRecord::Base
   has_many :loans
+
+  def funds_raised
+    loans.sum(:amount)
+  end
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -3,3 +3,13 @@ require 'spec_helper'
 describe Campaign, 'Associations' do
   it { expect(subject).to have_many(:loans) }
 end
+
+describe Campaign, '#funds_raised' do
+  it 'sums up the loans' do
+    campaign = create(:campaign)
+    create_list(:loan, 5)
+    create_list(:loan, 5, campaign: campaign)
+
+    expect(campaign.funds_raised).to eq 50
+  end
+end
